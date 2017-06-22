04332dfde93fcaeef2d5bb1343da5a8c7a31208f
kebin <kekobin@163.com>
2017-06-22 15:01:18 +0800

    update.

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ fis.set('modules.commands', ['init', 'install', 'release']);
 fis.set('template', '/views');
 fis.set('static', '/static');
 fis.set('config', '/config');
-fis.set('project.fileType.text', 'es,ts,tsx,jsx');
+fis.set('tmp', '/tmp');
+
 fis.set('project.ignore', [
     'favicon.ico',
     'README.md',
@@ -19,128 +20,127 @@ fis.set('project.ignore', [
     'output/**',
     '/client/node_modules/**',
     'fis-conf.js',
-    '/static/sass/**'
+    '/static/sass/_*.scss'
 ]);
 
 var clientRoadmap = {
-    // all release to $static dir
-    '/page/(**)': {
-        id: '$1',
-        moduleId: '${namespace}:page/$1',
-        url: '${namespace}/page/$1',
-        preprocessor: fis.plugin('extlang'),
-        postprocessor: fis.plugin('require-async'),
-        useMap: true,
-        useSameNameRequire: true,
-        extras: {
-            isPage: true
-        },
+    '/(**.html)': {
         release: '/${template}/$1'
     },
-    '/static/(**)': {
-        id: '$1',
-        moduleId: '${namespace}:$1',
-        release: '/${static}/$1'
-    },
-    '/static/**.sass': {
-        parser: fis.plugin('sass'),
+    '/sass/(**.{scss,sass})': {
+        parser: fis.plugin('node-sass'),
+        isCssLike: true,
+        useSprite: true,
+        release: '/css/$1',
         rExt: '.css'
     },
-    '/static/{**.ts,**.tsx,**.jsx,**.es}': {
-        parser: fis.plugin('typescript', {
-            module: 1,
-            target: 0,
-            sourceMap: true
-        }),
-        rExt: 'js'
+    '/img/(**)': {
+        release: '/img/$1'
     },
-    '/static/(widget/**.tpl)': {
-        url: '${namespace}/$1',
-        release: '/${template}/${namespace}/$1',
+    '/js/mod/(*.js)': {
+        moduleId: '$1',
+        isMod: true,
+        packTo: '/js/common.js'
+    },
+    '/widget/**': {
+        useSameNameRequire: true
+    },
+    '/widget/(**.tpl)': {
+        release: '/template/$1',
+        isHtmlLike: true
+    },
+    '/widget/**/(*.{png,jpg,jpeg,gif})': {
+        release: '/img/$1'
+    },
+    '/widget/**/(*).js': {
+        moduleId: '$1',
+        packTo: '/js/widget.js',
         useMap: true
     },
-    '/static/{components,widget}/**.{js,es,ts,tsx,jsx,css,less}': {
-        isMod: true
+    '/widget/**/(*.{scss,sass})': {
+        parser: fis.plugin('node-sass'),
+        isCssLike: true,
+        packTo: '/css/widget.css',
+        useMap: true,
+        rExt: '.css'
     },
-    '${namespace}-map.json': {
-        release: '${config}/${namespace}-map.json'
-    },
-    '::package': {}
-};
-
-var prodRoadmap = {
-    '/**.{js,css,less,ts,jsx,es,tsx}': {
-        useHash: true
-    },
-    '/**.{js,ts,jsx,es,tsx}': {
-        optimizer: fis.plugin('uglify-js')
-    },
-    '/**.{css,sass}': {
-        optimizer: fis.plugin('clean-css')
-    },
-    '::image': {
-        useHash: true
-    },
-    '/**.png': {
-        optimizer: fis.plugin('png-compressor')
+    '::package': {    // npm install [-g] fis3-postpackager-loader (doc: https://github.com/fex-team/fis3-postpackager-loader)
+        postpackager: fis.plugin('loader', {
+            obtainScript: false,
+            obtainStyle: false,
+            resourceType: 'commonJs',
+            sourceMap: true,
+            useInlineMap: true,
+            allInOne: true
+        })
     }
 };
 
-// 添加自定义命令
-// fis.require._cache['command-run'] = require('./command/run.js');
+// 模块化
+fis.hook('commonjs')    // npm install [-g] fis3-hook-commonjs (doc: https://github.com/fex-team/fis3-hook-commonjs)
+.match('/{widget,js/mod}/**.{js,es,ts,tsx,jsx}', {
+    isMod: true
+});
 
-[clientRoadmap, prodRoadmap].forEach(function(roadmap) {
+[clientRoadmap].forEach(function(roadmap) {
     fis.util.map(roadmap, function(selector, rules) {
         fis.match(selector, rules);
     });
 });
 
-// 发布模式关闭sourceMap
-fis.media('prod').match('/{**.ts,**.tsx,**.jsx,**.es}', {
-    parser: fis.plugin('typescript', {
-        module: 1,
-        target: 0
-    }),
-    rExt: 'js'
-});
-// 模块化支持
-fis.hook('commonjs', {
-    extList: ['.js', '.es', '.ts', '.tsx', '.jsx']
+var receiver = 'http://127.0.0.1:8085/axletree/upload';
+
+fis.media('debug').match('**', {
+    optimizer: null,
+    useHash: false,
+    deploy: fis.plugin('http-push', {
+        receiver: receiver,
+        to: '/static/'
+    })
+}).match('widget/**.tpl', {
+    deploy: fis.plugin('http-push', {
+        receiver: receiver,
+        to: '/views/'
+    })
+}).match('/(**.html)', {
+    deploy: fis.plugin('http-push', {
+        receiver: receiver,
+        to: '/'
+    })
 });
 
-// map.json
-fis.match('::package', {
-    postpackager: function createMap(ret, conf, settings, opt) {
-        var maps = {};
-        fis.util.map(ret.src, function(subpath, file) {
-            maps[file.id] = file;
-        });
-        var pkgMaps = {};
-        fis.util.map(ret.pkg, function(subpath, file) {
-            pkgMaps[file.getUrl()] = file;
-        });
-        var path = require('path');
-        var root = fis.project.getProjectPath();
-        var map = fis.file.wrap(path.join(root, fis.get('namespace') + '-map.json'));
-        var resKeys = Object.keys(ret.map.res);
-        var pkgKeys = Object.keys(ret.map.pkg);
-        for (var i = 0; i < resKeys.length; i++) {
-            var resId = resKeys[i];
-            if (maps[resId]) {
-                ret.map.res[resId].subpath = maps[resId].getHashRelease();
-            } else {
-                fis.log.warning(resId + ' is missing');
-            }
-        }
-        for (var j = 0; j < pkgKeys.length; j++) {
-            var pkg = ret.map.pkg[pkgKeys[j]];
-            if (pkgMaps[pkg.uri]) {
-                pkg.subpath = pkgMaps[pkg.uri].getHashRelease();
-            } else {
-                fis.log.warning(pkg.uri + ' is missing');
-            }
-        }
-        map.setContent(JSON.stringify(ret.map, null, 4));
-        ret.pkg[map.subpath] = map;
-    }
+var projectName = fis.get('projectName');
+
+// 生产环境
+fis.media('prod')
+.match('!{*.{html,tpl}, js/mod/**}', {
+    domain: 'http://hd.huya.com'+projectName,
+    deploy: fis.plugin('local-deliver', {
+        to: '../pub/'+projectName+'/'
+    })
+})
+.match('widget/**.tpl', {
+    deploy: fis.plugin('local-deliver', {
+        to: '../pub/'+projectName+'View/'
+    })
+}).match('/(**.html)', {
+    release: '/$1',
+    deploy: fis.plugin('local-deliver', {
+        to: '../pub/'+projectName+'View/'
+    })
+})
+
+//BASE
+.match('*.{png,js,scss,css}', {
+  useHash: true
+})
+.match('*.js', {
+    optimizer: fis.plugin('uglify-js')
+})
+.match('*.{css,scss,sass}', {
+    optimizer: fis.plugin('clean-css')
+})
+.match('*.png', {
+    optimizer: fis.plugin('png-compressor')
 });
+
